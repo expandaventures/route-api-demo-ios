ac532010bd3fde61ac9c0e3da9c39f62ae6fa27b
Daniel Garcia <gamdan89@gmail.com>
2016-04-27 12:25:11 -0500

    Added Options class|

--- SinTraficoRouteAPIDemo/SinTraficoRouteAPIDemo/StructOptions.h
+++ SinTraficoRouteAPIDemo/SinTraficoRouteAPIDemo/StructOptions.h
@@ -2,12 +2,23 @@
 //  StructOptions.h
 //  SinTraficoRouteAPIDemo
 //
-//  Created by SinTrafico on 4/27/16.
+//  Created by Daniel Garcia on 4/27/16.
 //  Copyright © 2016 SinTrafico. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
 
 @interface StructOptions : NSObject
+
+@property (assign) CLLocationCoordinate2D start;
+@property (assign) CLLocationCoordinate2D end;
+@property (retain, nonatomic) NSNumber* transportType;
+@property (retain, nonatomic) NSNumber* vehicleType;
+@property (assign) bool showParking;
+@property (assign) bool showTolls;
+@property (assign) bool showGas;
+
+- (NSDictionary*) parameters;
 
 @end
--- SinTraficoRouteAPIDemo/SinTraficoRouteAPIDemo/StructOptions.m
+++ SinTraficoRouteAPIDemo/SinTraficoRouteAPIDemo/StructOptions.m
@@ -2,12 +2,16 @@
 //  StructOptions.m
 //  SinTraficoRouteAPIDemo
 //
-//  Created by SinTrafico on 4/27/16.
+//  Created by Daniel Garcia on 4/27/16.
 //  Copyright © 2016 SinTrafico. All rights reserved.
 //
 
 #import "StructOptions.h"
 
 @implementation StructOptions
+
+- (NSDictionary*) parameters {
+    return @{@"test": @"test"};
+}
 
 @end
